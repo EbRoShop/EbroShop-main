2c6cea9b84858ddcab0fc46e9f2b3917f64bf03a
Realme <rebyidejene8949@gmail.com>
2026-01-04 07:50:23 +0300

    Add files via upload

--- db.php
+++ db.php
@@ -30,7 +30,7 @@ $host = getenv('DB_HOST');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
 $port = getenv('DB_PORT');
-$dbname = "ebroshop-db";
+$dbname = "defaultdb";
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
